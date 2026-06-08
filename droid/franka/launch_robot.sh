dfec0833bb9a479ea3658f530d5c5ddea0c09c1c
luyingxi35 <lu-yx23@mails.tsinghua.edu.cn>
2026-06-08 10:01:39 +0800

    update config

--- droid/franka/launch_robot.sh
+++ droid/franka/launch_robot.sh
@@ -2,4 +2,5 @@ source /home/robot/anaconda3/etc/profile.d/conda.sh
 conda activate polymetis-local
 pkill -9 run_server
 pkill -9 franka_panda_cl
-launch_robot.py robot_client=franka_hardware
+launch_robot.py ip=0.0.0.0 port=50051 robot_client=franka_hardware robot_client.executable_cfg.control_ip=127.0.0.1 robot_client.executable_cfg.control_port=50051
+
